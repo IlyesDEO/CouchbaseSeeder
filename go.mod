e67e71dad1a2484b710a17225e4cf0ff53f68c7f
ilyes <ilyes.deochandiano@ynov.com>
2023-06-13 14:41:41 +0200

    couchbasSeeder db

--- go.mod
+++ go.mod
@@ -8,7 +8,12 @@ require (
 )
 
 require (
-	github.com/golang/snappy v0.0.3 // indirect
+	github.com/couchbase/gocb/v2 v2.6.3 // indirect
+	github.com/couchbase/gocbcore/v10 v10.2.3 // indirect
+)
+
+require (
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	github.com/joho/godotenv v1.5.1
 	gopkg.in/inf.v0 v0.9.1 // indirect
--- main.go
+++ main.go
@@ -2,40 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
-	"github.com/kilianp07/CassandraSeeder/pkg/cassandra"
+	"github.com/kilianp07/CassandraSeeder/pkg/couchbase"
 	"github.com/kilianp07/CassandraSeeder/pkg/reader"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	data, err := reader.Read("./contacts.csv")
 	if err != nil {
 		panic(err)
 	}
-
-	// Initialize cassandra
-	cassandra, err := cassandra.NewCassandra(os.Getenv("CASSANDRA_HOST"), os.Getenv("CASSANDRA_USERNAME"), os.Getenv("CASSANDRA_PASSWORD"), "contacts")
-	if err != nil {
-		panic(err)
-	}
-	if err := cassandra.Initialize(); err != nil {
-		panic(err)
-	}
-
-	if err := cassandra.Migrate(); err != nil {
-		panic(err)
-	}
-
+	db := couchbase.NewCouchbase("localhost", "contact", "Administrator", "root123")
 	for _, contact := range data {
 		fmt.Println("Migrating Contact: ", contact.Name)
-		if err := cassandra.MigrateData(contact); err != nil {
+		if err := db.MigrateData(contact); err != nil {
 			panic(err)
 		}
 	}
